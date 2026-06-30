44a5c3e95287d423a32de063d45c8adf4e4a1eae
Rowland Chazima <apungurowland90@gmail.com>
2026-06-30 09:03:45 +0300

    Project Set Up

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Register from "./components/Register";
+
+const App = () => {
+  return (
+    <div className="min-h-screen bg-[#f7f6f0] text-[#2d3748] antialiased">
+      <Register />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Register.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Register = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center  p-5 bg-[#f7f6f0]">
+      <div className="bg-white p-10 rounded-[24px] w-full max-w-md shadow-[0_10px_30px_rgba(0,0,0,0.02)]">
+        <div className="flex justify-between items-center mb-2">
+          <h1>Registration Form</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
